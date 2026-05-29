dd36280d945889afa177f3e38dcd44e37c821cdf
manikandan2025cse-oss <skartikeyan29@gmail.com>
2026-05-29 21:47:45 +0530

    deployment ready

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-import os
 from pymongo import MongoClient
+import os
 
 MONGO_URI = os.getenv("MONGO_URI")
 SECRET_KEY = os.getenv("SECRET_KEY", "secret123")
 
 client = MongoClient(MONGO_URI)
+
 db = client["grievance_portal"]
 
-complaints = db["complaints"]
+complaints_collection = db["complaints"]
